e3d65568f79c44a9edb1c051a69c1ba6f6ec46f7
Sil de la Barrera <sil.delabarrera@hotmail.com>
2025-02-16 19:31:14 +0100

    sidebar sin categorias

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [CommonModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+  isSidebarOpen = false; // Estado para abrir/cerrar la sidebar
+  openCategories: Set<string> = new Set(); // Para gestionar las categorías abiertas
+
+  // Función para abrir y cerrar la sidebar
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  // Función para abrir/cerrar una categoría
+  toggleCategory(category: string) {
+    if (this.openCategories.has(category)) {
+      this.openCategories.delete(category);
+    } else {
+      this.openCategories.add(category);
+    }
+  }
+
+  // Verificar si una categoría está abierta
+  isCategoryOpen(category: string): boolean {
+    return this.openCategories.has(category);
+  }
+}
